d72749ab24c0faaca6438e3dac8dc2a50826b387
relish <guptarelish333@gmail.com>
2025-06-27 00:02:22 +0530

    added videos and images

--- app/projects/aurora-abode/page.js
+++ app/projects/aurora-abode/page.js
@@ -1,6 +1,6 @@
 import StickyImageStack from "@/components/StickyImageStack"
 
-const imageCount = 23; 
+const imageCount = 58; 
 const videoNumbers = [1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 const images = Array.from({ length: imageCount }, (_, i) =>
