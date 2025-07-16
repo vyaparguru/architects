c1fd827e6f617fd36016e2e2e744bc24d7fae86f
relish <guptarelish333@gmail.com>
2025-07-16 22:00:34 +0530

    added photos in projects

--- app/projects/shiv-jyoti-public-school/page.js
+++ app/projects/shiv-jyoti-public-school/page.js
@@ -5,7 +5,7 @@ const videoNumbers = [1,2,3,4,5,6,7]
   // ,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 const images = Array.from({ length: imageCount }, (_, i) =>
-  `/projects/shiv-jyoti/${i + 1}.JPG`
+  `/projects/shiv-jyoti/${i + 1}.jpg`
 );
 
 const videos = videoNumbers.map(
@@ -15,7 +15,7 @@ const videos = videoNumbers.map(
 const ShivJyoti = () => {
   return (
     <StickyImageStack
-      topImage="/projects/12.JPG"
+      topImage="/projects/12.jpg"
       projectName="Shiv Jyoti Public School"
       images={images}
       videos={videos}
